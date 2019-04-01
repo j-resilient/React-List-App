56552ffa1af4ba8d129fd272040234bc475a2e63
Wish McCarty <joannamccarty10@gmail.com>
2019-03-31 20:53:06 -0400

    start to add src files

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import Lists from './Lists.js';
+import AddList from './AddList.js';
 import './App.css';
 
 class App extends Component {
